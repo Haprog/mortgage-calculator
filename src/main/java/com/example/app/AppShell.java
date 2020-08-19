f706be830f2211525f16dbb09fbd998c83b8c9ca
Kari Söderholm <haprog@vaadin.com>
2020-08-19 10:41:29 +0300

    Upgrade to 17.0.0.beta1 and Flow 4.1-SNAPSHOT

--- src/main/java/com/example/app/AppShell.java
+++ src/main/java/com/example/app/AppShell.java
@@ -3,7 +3,7 @@ package com.example.app;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 
-@PWA(name = "Offline Form", shortName = "Offline Form", offlineResources = { "**" }, enableInstallPrompt = false)
+@PWA(name = "Offline Form", shortName = "Offline Form", offlineResources = { "**" })
 public class AppShell implements AppShellConfigurator {
 
 }
